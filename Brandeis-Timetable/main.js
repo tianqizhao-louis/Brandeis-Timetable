1cde541b1d0f1b2c4e71f134c23674cdcc383a76
Julian Berlin <julianb@brandeis.edu>
2020-06-10 16:13:07 -0400

    re adding adrews work

--- Brandeis-Timetable/main.js
+++ Brandeis-Timetable/main.js
@@ -40,7 +40,34 @@ app.get("/test_prof_profile",homeController.showTestProf);
 app.get("/test_class_schedule",homeController.showTestSchedule);
 app.post("/contact", homeController.postedSignUpForm);
 
+const Contact=require("./models/Contact")
+app.get("/showContacts",
+ async(req,res) => {
+   try{
+     res.locals.contacts = await Contact.find({})
+     res.render('showContacts')
+   }
+   catch(e) {
+     console.log("Error:"+e)
+     console.dir(theError)
+     res.send("There was an error in /showContacts!")
+   }
+ })
 
+app.post('Contact',
+  async(req,res) => {
+    try {
+      let name = req.body.name
+      let email = req.body.email
+      let message = req.body.message
+      let newContact = new Contact({name:name, email:email, message:message})
+      await newContact.save()
+      res.redirect('/showContacts')
+    }
+    catch(e) {
+      res.send("error in addContact")
+    }
+  });
 app.use(errorController.pageNotFoundError);
 app.use(errorController.internalServerError);
 
