e226d5528a36dc6688d964bccd77ad59f78b181f
Looouis <louiszhao0517@yahoo.com>
2020-06-14 22:24:18 -0400

    update

--- Test_Pages/index.js
+++ Test_Pages/index.js
@@ -21,3 +21,25 @@ app.get("/professor_schema", professorController.getAllProfessors,
   console.log(req.data);
   res.send(req.data);
 });
+
+
+// trigger
+app.set("view engine", "ejs");
+app.set("port", process.env.PORT || 3000);
+app.use(
+  express.urlencoded({
+    extended: false
+  })
+);
+app.use(express.json());
+//app.use(layouts);
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+
+app.listen(app.get("port"), () => {
+  console.log(`Server running at http://localhost:${app.get("port")}`);
+});
--- Test_Pages/models/professor_schema.js
+++ Test_Pages/models/professor_schema.js
@@ -7,7 +7,59 @@ const mongoose = require("mongoose"),
     email: String,
     courses: [Number], // course ids
     prof_id: Number, // an unique id
-    schedule:[String:[Number:Boolean]]// Monday: 9 : True
+    schedule:{
+      Monday : {
+        9 : Boolean,
+        10 : Boolean,
+        11: Boolean,
+        12: Boolean,
+        13: Boolean,
+        14: Boolean,
+        15: Boolean,
+        16: Boolean
+      },
+      Tuesday: {
+        9 : Boolean,
+        10 : Boolean,
+        11: Boolean,
+        12: Boolean,
+        13: Boolean,
+        14: Boolean,
+        15: Boolean,
+        16: Boolean
+      },
+      Wednesday: {
+        9 : Boolean,
+        10 : Boolean,
+        11: Boolean,
+        12: Boolean,
+        13: Boolean,
+        14: Boolean,
+        15: Boolean,
+        16: Boolean
+      },
+      Thursday: {
+        9 : Boolean,
+        10 : Boolean,
+        11: Boolean,
+        12: Boolean,
+        13: Boolean,
+        14: Boolean,
+        15: Boolean,
+        16: Boolean
+      },
+      Friday: {
+        9 : Boolean,
+        10 : Boolean,
+        11: Boolean,
+        12: Boolean,
+        13: Boolean,
+        14: Boolean,
+        15: Boolean,
+        16: Boolean
+      }
+    }
+    //[String:[Number:Boolean]]// Monday: 9 : True
   });
 
 
