c233d3fada035335a4e2f17e74f0f730310dc52d
achen2018 <acheny@gmail.com>
2020-06-20 14:43:20 -0400

    Revert "Merge branch 'master' of https://github.com/Looooouis/Brandeis-Timetable"
    This reverts commit 1996bbf15ab087d36358bbbe7f08507f23c25d18, reversing
    changes made to 9daff17a9cc2b1b7b559d6ee97dd2fdb3ff4edf0.

--- Brandeis-Timetable/controllers/homeController.js
+++ Brandeis-Timetable/controllers/homeController.js
@@ -56,9 +56,6 @@ exports.showSignUp = (req, res) => {
     date: new Date(),
   });
 };
-exports.showgridEditor = (req, res) => {
-  res.render("gridEditor");
-};
 
 exports.postedSignUpForm = (req, res) => {
   let formData = req.body
--- Brandeis-Timetable/models/Grid.js
+++ Brandeis-Timetable/models/Grid.js
@@ -3,13 +3,15 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
 
 const gridSchema = Schema( {
-  prof_name: String,
-  department: String,
-  courseid: String,
-  prof_hours: String,
-  prof_office: String,
+  //department: String,
+  //prof_name: String,
+  //prof_hours: String,
+  //prof_office: String,
+  //courseid: String,
   //tas: [{ta_name: String, ta_hours: String}],
   //bugs: [{bugs_name: String, bugs_hous: String}]
+  a: String,
+  b: String
 });
 
 module.exports = mongoose.model('Grid', gridSchema);
